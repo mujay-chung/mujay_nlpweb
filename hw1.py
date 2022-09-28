9f6cfe7ecb61e14dd9667d2cc748b09831322344
mujay-chung <113580482+mujay-chung@users.noreply.github.com>
2022-09-28 17:33:47 +0800

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -1,14 +1,7 @@
 # R10521517 鍾慕捷
 
 import streamlit as st
-import seaborn as sns 
 import matplotlib.pyplot as plt 
-import pandas as pd 
-import pickle 
-from sklearn.metrics import accuracy_score 
-from sklearn.ensemble import RandomForestClassifier 
-from sklearn.model_selection import train_test_split
-from textblob import TextBlob
 from snownlp import SnowNLP
 import time
 
